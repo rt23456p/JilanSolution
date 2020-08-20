cfebe6a0b2a599dc28423a4ef1287f8f8c998167
Leo <rt23456p@gmail.com>
2020-08-20 12:51:47 +0800

    Update Process.py

--- Process.py
+++ Process.py
@@ -7,31 +7,37 @@ code=code
 
 
 
-
-
-
-
-
-
 #Code Section, don't touch it unless you know python well!
 
 
-##Longer Term Para
 
 
+    ##Repleace Counter
+
+repleace_works=0
+int(repleace_works)
 
 
+#Longer Term Para Section
 
-##Short Term Para
+
+#Short Term Para
+
+
+        ###General English
 if 'An example of' in code:
     code = code.replace('An example of', 'An illustration of')
-
+    print("hi")
 if 'an example of' in code:
     code = code.replace('an example of', 'an illustration of')
 
 if 'faced some issues' in code:
     code = code.replace('faced some issues', 'faced a variety of issue')
 
+
+
+
+        ###Aerospace
 if 'Space Shuttle Challenger' in code:
     code = code.replace('Space Shuttle Challenger', 'American Shuttle Challenger')
 
@@ -39,16 +45,15 @@ if 'Space Shuttle Columbia' in code:
     code = code.replace('Space Shuttle Columbia', 'American Shuttle Columbia')
 
 
-##SIngle/Dual Word para
-if ''
+    ##Single/Dual Word para
+if ' ' in code:
+    pass
 
 
 #Remove this later, just for testing purposes
 if 'tesla' in code:
     code = code.replace('tesla', 'BMW')
-
-
-
+    repleace_works = repleace_works + 1
 
 
 
@@ -56,6 +61,6 @@ if 'tesla' in code:
 #Output section
 print(" ")
 print(" ")
-print("Work Done:")
-print(" ")
+print("Work Done: We repleaced a total of " + str(repleace_works) + " words/short terms")
+print("Here is the output in the next line")
 print(code)
