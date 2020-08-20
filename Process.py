fd15abb140bdea6014ba9607ab2f7aad7b994834
Leo <rt23456p@gmail.com>
2020-08-20 11:32:06 +0800

    Update Process.py

--- Process.py
+++ Process.py
@@ -8,12 +8,15 @@ code=code
 
 
 
-#A1 test product
+#Code Section, don't touch it unless you know python well!
 if 'An example of' in code:
     code = code.replace('An example of', 'An illustration of')
 
 if 'an example of' in code:
     code = code.replace('an example of', 'an illustration of')
+
+if 'faced some issues' in code:
+    code = code.replace('faced some issues', 'faced a variety of issue')
 
 if 'tesla' in code:
     code = code.replace('tesla', 'BMW')
