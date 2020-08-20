52cdd8b3586ea75152b69bfa69960d193826d938
Leo <rt23456p@gmail.com>
2020-08-20 12:37:18 +0800

    Update Process.py

--- Process.py
+++ Process.py
@@ -8,7 +8,21 @@ code=code
 
 
 
+
+
+
+
+
 #Code Section, don't touch it unless you know python well!
+
+
+##Longer Term Para
+
+
+
+
+
+##Short Term Para
 if 'An example of' in code:
     code = code.replace('An example of', 'An illustration of')
 
@@ -18,13 +32,25 @@ if 'an example of' in code:
 if 'faced some issues' in code:
     code = code.replace('faced some issues', 'faced a variety of issue')
 
+if 'Space Shuttle Challenger' in code:
+    code = code.replace('Space Shuttle Challenger', 'American Shuttle Challenger')
+
+if 'Space Shuttle Columbia' in code:
+    code = code.replace('Space Shuttle Columbia', 'American Shuttle Columbia')
+
+
+##SIngle/Dual Word para
+if ''
+
+
+#Remove this later, just for testing purposes
 if 'tesla' in code:
     code = code.replace('tesla', 'BMW')
 
 
 
 
-#B
+
 
 
 #Output section
