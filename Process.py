84b7c1856eff96e70667e01d306432fcc3076708
Leo <rt23456p@gmail.com>
2020-08-21 09:29:28 +0800

    Update Process.py

--- Process.py
+++ Process.py
@@ -1,6 +1,13 @@
 
 
-#Import saved code file
+
+
+#import all resources needed
+
+import random
+
+
+#Import user entered code file
 from usercode import code
 code=code
 
@@ -10,21 +17,22 @@ code=code
 #Code Section, don't touch it unless you know python well!
 
 
-
-
-    ##Number Counter for user display
+    ##Number Repleace Counter for user display
 repleace_works=0
 int(repleace_works)
+    ##Word Count for user input
+word_counter = len(code.split())
 
 
-
-    ##Remove this later, just for testing purposes
-if 'tesla' in code:
-    code = code.replace('tesla', 'BMW')
-    repleace_works = repleace_works + 1
-
-
-
+    ##Remove this later, just for testing purposes, remove later
+while 'tesla' in code:
+    rangenint = random.randint(1,3)
+    if rangenint == 1:
+        code = code.replace('tesla', 'BMW',1)
+    if rangenint == 2:
+        code = code.replace('tesla', 'porsche',1)
+    if rangenint == 3:
+        code = code.replace('tesla', 'Honda',1)
 
     ##Longer Term Para Section
 
@@ -33,15 +41,25 @@ if 'tesla' in code:
 
 
         ###General English
-if 'An example of' in code:
-    code = code.replace('An example of', 'An illustration of')
-    print("hi")
-if 'an example of' in code:
-    code = code.replace('an example of', 'an illustration of')
+
+
+#UM-1
+while 'An example of' in code:
+    rangenint = random.randint(1,2)
+    if rangenint == 1:
+        code = code.replace('An example of', 'An illustration of',1)
+    if rangenint == 2:
+        code = code.replace('An example of', 'A case in point of',1)
+while 'an example of' in code:
+    rangenint = random.randint(1,2)
+    if rangenint == 1:
+        code = code.replace('an example of', 'an illustration of',1)
+    if rangenint == 2:
+        code = code.replace('an example of', 'a case in point of',1)
+
 
 if 'faced some issues' in code:
     code = code.replace('faced some issues', 'faced a variety of issue')
-
 
 
 
@@ -63,6 +81,7 @@ if ' ' in code:
 #Output section
 print(" ")
 print(" ")
+print ("The number of words you enter are : " + str(word_counter))
 print("Work Done: We repleaced a total of " + str(repleace_works) + " words/short terms")
-print("The output in the next line")
+print("View your output in the next line")
 print(code)
