37822d0c79bd29b4301454697ef100360fbe30c8
Leo <rt23456p@gmail.com>
2020-08-21 12:10:05 +0800

    Update usercode.py

--- usercode.py
+++ usercode.py
@@ -2,6 +2,5 @@
 
 
 
-
 #Below is the code section, by default, it should be Airbus is an example of a muti-billion International corporation,
-code= "Airbus is an example of a muti-billion International corporation  trump"
+code= "Airbus is trump"
