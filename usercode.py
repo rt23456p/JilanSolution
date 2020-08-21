51cfa44d6723a18b7186b186ea314f9d36c5ba07
Leo <rt23456p@gmail.com>
2020-08-21 11:25:36 +0800

    Update usercode.py

--- usercode.py
+++ usercode.py
@@ -4,4 +4,4 @@
 
 
 #Below is the enter section, by default, it should be tesla, and running this software will output you a code of BMW.
-code= "tesla "
+code= "Airbus is an example of a muti-billion International corporation"
