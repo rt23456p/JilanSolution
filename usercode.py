8f1427694d35c27f11a23d41a64dd8dc8c152f6d
Leo <rt23456p@gmail.com>
2020-08-21 12:56:42 +0800

    Update usercode.py

--- usercode.py
+++ usercode.py
@@ -2,5 +2,5 @@
 
 
 
-#Below is the code section, by default, it should be Airbus is an example of a muti-billion International corporation,
-code= "Airbus is International corporation ~JilanSolution_Sudo"
+#Below is the code section,
+code= " sudo ~JilanSolution_Sudo"
