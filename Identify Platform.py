192b33a95e5d56f5ea797eba9f989b2f7b290c33
Leo Liang <rt23456p@ya.ru>
2020-09-02 12:46:34 +0800

    Create Identify Platform.py

---
+++ Identify Platform.py
@@ -0,0 +1,17 @@
+#Identifier Test Platform, Only for feedback to improve on Process.py, not for final release
+
+
+
+
+#So the Purpose of this is to make two way repleace possible, as the process.py still have one way repleace, so the other way will not work, but with this been used as planned, it should word
+
+# Test Var
+subject= 'summit'
+
+# Triggering subject
+trigger = 0
+
+if trigger == 1 in subject:
+    #Notify on the test Platform, final will not have this
+    print("Trigger Active")
+else:
