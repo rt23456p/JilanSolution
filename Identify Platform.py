954e5071cee8479df5dd9d175aaee9fbeeacbda1
rt23456p <rt23456p@yandex.ru>
2020-09-03 18:50:33 +0800

    Update Identify Platform.py

--- Identify Platform.py
+++ Identify Platform.py
@@ -1,30 +1,28 @@
 #Identifier Test Platform, Only for feedback to improve on Process.py, not for final release
 
 
+#Update Sep 3 2020, this test is finished successfully today, therefore, this is the final version
+
+#So the Purpose of this is to make two way repleace possible, as the process.py still have one way repleace, so the other way will not work, but with this been used as planned, it should worl both ways.
 
 
-#So the Purpose of this is to make two way repleace possible, as the process.py still have one way repleace, so the other way will not work, but with this been used as planned, it should word
-
-# Test Var
+# Test Var# Triggering subject
+trigger = "1"
 subject= 'summit'
-
-# Triggering subject
-trigger = 0
-
 
 if 'summit' in subject:
     subject = subject.replace('summit', 'peak')
     print("Trigger Active(0 Pos)")
-    trigger == 'hash'
+    trigger == '2'
 
 
-if trigger == 'hash':
+if trigger == '1':
     #Notify on the test Platform, final will not have this
-    print("Trigger Active (1 pos)")
+    print("1")
+    subject = subject.replace('peak', 'summit')
 
 else:
-    if 'peak' in subject:
-        subject = subject.replace('peak', 'summit')
+    print("Trigger Active (1 pos)")
 
 
 
