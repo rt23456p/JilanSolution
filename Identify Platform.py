42fa44cb1a45e7242264ee271b38e41eda6dafdb
Leo Liang <rt23456p@ya.ru>
2020-09-04 11:17:11 +0800

    Update Identify Platform.py

--- Identify Platform.py
+++ Identify Platform.py
@@ -1,7 +1,7 @@
 #Identifier Test Platform, Only for feedback to improve on Process.py, not for final release
 
 
-#Update Sep 3 2020, this test is finished successfully today, therefore, this is the final version
+#Update Sep 4 2020, this test is finished successfully today, therefore, this is the final version
 
 #So the Purpose of this is to make two way repleace possible, as the process.py still have one way repleace, so the other way will not work, but with this been used as planned, it should worl both ways.
 
@@ -13,7 +13,7 @@ subject= 'summit'
 if 'summit' in subject:
     subject = subject.replace('summit', 'peak')
     print("Trigger Active(0 Pos)")
-    trigger == '2'
+    trigger = '2'
 
 
 if trigger == '1':
